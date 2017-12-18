b3a575aceeddc699a4075ad201c460b509428503
Albert Zheng <albertzheng@Alberts-MacBook-Pro.local>
2017-12-18 19:42:34 +1300

    Add training model

---
+++ train_model.py
@@ -0,0 +1,33 @@
+import csv
+import cv2
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+lines = []
+# open up csv file which contains driving logs
+with open('../data/driving_logs.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader: 
+		lines.append(line) # append in each line of csv data into list
+
+images = []
+measurements = []
+
+for line in lines:
+	source_path = line[0]
+	filename = source_path.split('/')[-1] # get filename from path
+	curr_path = '../data/IMG/' + filename
+	image = cv2.imread(curr_path)
+	images.append(images)
+	measurements.append(float(line[3]))
+
+y_train = np.array(measurements)
+
+model = Sequential() # build sequential model
+model.add(Flatten(input_shape=160,320,3)) # flatten input into single array
+model.add(Dense(1)) # single dimensionality since input is flattened to N X 1 array
+
+model.compile(loss='mse',optimizer='adam')
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=7)
+model.save('model_test')
