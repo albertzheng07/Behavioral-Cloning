fe93ff9550b9aa7e65f7f475cc62090eed0ff866
Albert Zheng <albert.zheng@zee.aero>
2017-12-21 00:15:56 +1300

    Add cropping and correction factor on left right images

--- train_model.py
+++ train_model.py
@@ -2,9 +2,10 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+import ipdb
 
 lines = []
 # open up csv file which contains driving logs
@@ -16,24 +17,32 @@ with open('../data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 
-for line in lines[1:]:
-	source_path = line[0]
+def get_image_path(source_path):
 	filename = source_path.split('/')[-1] # get filename from path
 	curr_path = '../data/IMG/' + filename
-	image = cv2.imread(curr_path)
-	images.append(image)
-	image_flipped = np.fliplr(image)
-	images.append(image_flipped)
-	measurement = float(line[3])	
-	measurement_flipped = -measurement	
-	measurements.append(measurement)
-	measurements.append(measurement_flipped)	
+	return curr_path
+
+correction_factor = [0.0,-0.3,0.3]
+
+for line in lines[1:]:
+	for ind in range(3):
+		img_path = get_image_path(line[ind])
+		image = cv2.imread(img_path)
+		images.append(image)
+		image_flipped = np.fliplr(image)
+		images.append(image_flipped)
+		steering = float(line[3])+correction_factor[ind] # steering measurement
+		steering_flipped = -steering	
+		measurements.append(steering)
+		measurements.append(steering_flipped)	
+
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
-model = Sequential() # build sequential model
+model = Sequential() # build sequential model of LeNet
 model.add(Lambda(lambda x: x/255-0.5,input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0)))) # crop out the top 75 pixels, bottom 25 pixels, none n the outside
 model.add(Convolution2D(15,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(15,5,5,activation="relu"))
@@ -44,6 +53,6 @@ model.add(Dense(84))
 model.add(Dense(1)) # single dimensionality since input is flattened to N X 1 array
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=10)
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=5)
 model.save('model_test')
 exit()
