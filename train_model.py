0eb1a40e707d56aa71e9efc8a7a8bca35d45bbde
Albert Zheng <albert.zheng@zee.aero>
2017-12-20 23:37:32 +1300

    Increase filter size and add flipped images for training

--- train_model.py
+++ train_model.py
@@ -22,16 +22,21 @@ for line in lines[1:]:
 	curr_path = '../data/IMG/' + filename
 	image = cv2.imread(curr_path)
 	images.append(image)
-	measurements.append(float(line[3]))
+	image_flipped = np.fliplr(image)
+	images.append(image_flipped)
+	measurement = float(line[3])	
+	measurement_flipped = -measurement	
+	measurements.append(measurement)
+	measurements.append(measurement_flipped)	
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 model = Sequential() # build sequential model
 model.add(Lambda(lambda x: x/255-0.5,input_shape=(160,320,3)))
-model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(Convolution2D(15,5,5,activation="relu"))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(Convolution2D(15,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Flatten()) # flatten input into single array
 model.add(Dense(120)) 
@@ -39,6 +44,6 @@ model.add(Dense(84))
 model.add(Dense(1)) # single dimensionality since input is flattened to N X 1 array
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=7)
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,nb_epoch=10)
 model.save('model_test')
 exit()
